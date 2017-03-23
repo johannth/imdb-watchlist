d3859de8c5e886f94e301dcb1bbcdd13c01229f0
Jóhann Þorvaldur Bergþórsson <johann.thorvaldur@gmail.com>
2017-03-23 19:26:27 +0000

    Add throttle to not overwhelm Bechdel & Netflix

--- server/bechdel.js
+++ server/bechdel.js
@@ -1,26 +1,40 @@
 import fetch from 'node-fetch';
+import PromiseThrottle from 'promise-throttle';
+
 import { handleErrors } from './utils';
+
+const promiseThrottle = new PromiseThrottle({
+  requestsPerSecond: 5,
+  promiseImplementation: Promise,
+});
 
 export const fetchBechdel = imdbId => {
   const imdbIdWithoutPrefix = imdbId.replace('tt', '');
 
-  return fetch(
-    `http://bechdeltest.com/api/v1/getMovieByImdbId?imdbid=${imdbIdWithoutPrefix}`,
-    {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-      },
-    }
-  )
-    .then(handleErrors)
-    .then(response => response.json())
-    .then(json => {
-      if (!json.status) {
-        return { rating: parseInt(json.rating), dubious: json.dubious === '1' };
-      } else {
-        return null;
+  const startRequest = () => {
+    return fetch(
+      `http://bechdeltest.com/api/v1/getMovieByImdbId?imdbid=${imdbIdWithoutPrefix}`,
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
       }
-    });
+    )
+      .then(handleErrors)
+      .then(response => response.json())
+      .then(json => {
+        if (!json.status) {
+          return {
+            rating: parseInt(json.rating),
+            dubious: json.dubious === '1',
+          };
+        } else {
+          return null;
+        }
+      });
+  };
+
+  return promiseThrottle.add(startRequest);
 };
--- server/netflix.js
+++ server/netflix.js
@@ -1,43 +1,57 @@
 import fetch from 'node-fetch';
 import request from 'request';
+import PromiseThrottle from 'promise-throttle';
 import { handleErrors } from './utils';
 
+const promiseThrottle = new PromiseThrottle({
+  requestsPerSecond: 5,
+  promiseImplementation: Promise,
+});
+
 export const checkIfMovieIsOnLocalNetflix = (imdbId, title, locale) => {
-  return fetch(
-    `http://denmark.flixlist.co/autocomplete/titles?q=${encodeURIComponent(title)}`
-  )
-    .then(handleErrors)
-    .then(response => response.json())
-    .then(json => {
-      if (!json.filter) {
-        throw Error(`${imdbId}: ${title} was not found on '${locale}' Netflix`);
-      }
-
-      const possibleNetflixId = json
-        .filter(result => {
-          return result.title === title; // This missing a check for year but it still much better than nothing.
-        })
-        .map(result => {
-          return result.url.replace('/titles/', '');
-        })[0];
-
-      if (!possibleNetflixId) {
-        throw Error(`${imdbId}: ${title} was not found on '${locale}' Netflix`);
-      }
-
-      return doubleCheckIfMovieIsOnLocaleNetflix(
-        imdbId,
-        `https://www.netflix.com/title/${possibleNetflixId}`,
-        locale
-      ).then(netflixUrl => {
-        if (!netflixUrl) {
+  const startRequest = () => {
+    return fetch(
+      `http://denmark.flixlist.co/autocomplete/titles?q=${encodeURIComponent(title)}`
+    )
+      .then(handleErrors)
+      .then(response => response.json())
+      .then(json => {
+        if (!json.filter) {
           throw Error(
             `${imdbId}: ${title} was not found on '${locale}' Netflix`
           );
         }
-        return { netflixUrl };
+
+        const possibleNetflixId = json
+          .filter(result => {
+            return result.title === title; // This missing a check for year but it still much better than nothing.
+          })
+          .map(result => {
+            return result.url.replace('/titles/', '');
+          })[0];
+
+        if (!possibleNetflixId) {
+          throw Error(
+            `${imdbId}: ${title} was not found on '${locale}' Netflix`
+          );
+        }
+
+        return doubleCheckIfMovieIsOnLocaleNetflix(
+          imdbId,
+          `https://www.netflix.com/title/${possibleNetflixId}`,
+          locale
+        ).then(netflixUrl => {
+          if (!netflixUrl) {
+            throw Error(
+              `${imdbId}: ${title} was not found on '${locale}' Netflix`
+            );
+          }
+          return { netflixUrl };
+        });
       });
-    });
+  };
+
+  return promiseThrottle.add(startRequest);
 };
 
 const doubleCheckIfMovieIsOnLocaleNetflix = (imdbId, netflixUrl, locale) => {
