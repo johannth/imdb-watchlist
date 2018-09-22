27c214e4d83ec11f00ba1700eb8473f4539cbeff
Jóhann Þorvaldur Bergþórsson <johann.thorvaldur@gmail.com>
2018-09-22 13:55:54 +0000

    Fix netflix fetching

--- server/netflix.js
+++ server/netflix.js
@@ -1,73 +1,92 @@
-import fetch from 'node-fetch';
-import request from 'request';
-import PromiseThrottle from 'promise-throttle';
-import { handleErrors } from './utils';
+import fetch from "node-fetch";
+import request from "request";
+import PromiseThrottle from "promise-throttle";
+import { handleErrors } from "./utils";
 
 const promiseThrottle = new PromiseThrottle({
-  requestsPerSecond: 10,
-  promiseImplementation: Promise,
+	requestsPerSecond: 10,
+	promiseImplementation: Promise
 });
 
 export const checkIfMovieIsOnLocalNetflix = (imdbId, title, locale) => {
-  const locales = {
-    is: 'denmark',
-    de: 'germany',
-  };
+	const locales = {
+		ca: "",
+		is: "denmark",
+		de: "germany"
+	};
 
-  const startRequest = () =>
-    fetch(
-      `http://${locales[locale]}.flixlist.co/autocomplete/titles?q=${encodeURIComponent(title)}`,
-    )
-      .then(handleErrors)
-      .then(response => response.json())
-      .then((json) => {
-        if (!json.filter) {
-          throw Error(`${imdbId}: ${title} was not found on '${locale}' Netflix`);
-        }
+	const startRequest = () =>
+		fetch(
+			`http://${
+				locales[locale]
+			}.flixlist.co/autocomplete/titles?q=${encodeURIComponent(title)}`
+		)
+			.then(handleErrors)
+			.then(response => response.json())
+			.then(json => {
+				if (!json.filter) {
+					throw Error(
+						`${imdbId}: ${title} was not found on '${locale}' Netflix`
+					);
+				}
 
-        const possibleNetflixId = json
-          .filter(
-            result => result.title === title, // This missing a check for year but it still much better than nothing.
-          )
-          .map(result => result.url.replace('/titles/', ''))[0];
+				const possibleNetflixId = json
+					.filter(
+						result => result.title === title // This missing a check for year but it still much better than nothing.
+					)
+					.map(result => result.url.replace("/titles/", ""))[0];
 
-        if (!possibleNetflixId) {
-          throw Error(`${imdbId}: ${title} was not found on '${locale}' Netflix`);
-        }
+				if (!possibleNetflixId) {
+					throw Error(
+						`${imdbId}: ${title} was not found on '${locale}' Netflix`
+					);
+				}
 
-        return doubleCheckIfMovieIsOnLocaleNetflix(
-          imdbId,
-          `https://www.netflix.com/title/${possibleNetflixId}`,
-          locale,
-        ).then((netflixUrl) => {
-          if (!netflixUrl) {
-            throw Error(`${imdbId}: ${title} was not found on '${locale}' Netflix`);
-          }
-          return { netflixUrl };
-        });
-      });
+				return doubleCheckIfMovieIsOnLocaleNetflix(
+					imdbId,
+					`https://www.netflix.com/title/${possibleNetflixId}`,
+					locale
+				).then(netflixUrl => {
+					if (!netflixUrl) {
+						throw Error(
+							`${imdbId}: ${title} was not found on '${locale}' Netflix`
+						);
+					}
+					return { netflixUrl };
+				});
+			});
 
-  return promiseThrottle.add(startRequest);
+	return promiseThrottle.add(startRequest);
 };
 
 const doubleCheckIfMovieIsOnLocaleNetflix = (imdbId, netflixUrl, locale) => {
-  const netflixUrlInLocale = netflixUrl.replace('/title/', `/${locale}/title/`);
-  const netflixUrlInLocaleAlternative = netflixUrl.replace('/title/', `/${locale}-en/title/`);
+	const netflixUrlInLocale = netflixUrl.replace("/title/", `/${locale}/title/`);
+	const netflixUrlInLocaleAlternative = netflixUrl.replace(
+		"/title/",
+		`/${locale}-en/title/`
+	);
 
-  return new Promise((resolve, reject) => {
-    request({ method: 'GET', followRedirect: false, url: netflixUrl }, (error, response, body) => {
-      const locationHeader = response.headers.location;
-      console.log(
-        `/api/netflix ${imdbId}: Netflix returned ${response.statusCode} on ${netflixUrl} with location ${locationHeader}`,
-      );
+	console.log(netflixUrl, netflixUrlInLocale, netflixUrlInLocaleAlternative);
 
-      resolve(
-        response.statusCode == 200 ||
-          locationHeader === netflixUrlInLocale ||
-          locationHeader === netflixUrlInLocaleAlternative
-          ? netflixUrlInLocale
-          : null,
-      );
-    });
-  });
+	return new Promise((resolve, reject) => {
+		request(
+			{ method: "GET", followRedirect: false, url: netflixUrl },
+			(error, response, body) => {
+				const locationHeader = response.headers.location;
+				console.log(
+					`/api/netflix ${imdbId}: Netflix returned ${
+						response.statusCode
+					} on ${netflixUrl} with location ${locationHeader}`
+				);
+
+				resolve(
+					response.statusCode == 200 ||
+					locationHeader === netflixUrlInLocale ||
+					locationHeader === netflixUrlInLocaleAlternative
+						? netflixUrlInLocale
+						: null
+				);
+			}
+		);
+	});
 };
--- server/server.js
+++ server/server.js
@@ -1,120 +1,130 @@
-import newrelic from 'newrelic';
-import express from 'express';
-import bodyParser from 'body-parser';
+import newrelic from "newrelic";
+import express from "express";
+import bodyParser from "body-parser";
 
-import cors from 'cors';
-import { createCache } from './cache';
-import { fetchImdbWatchList } from './imdb';
-import { fetchBechdel } from './bechdel';
-import { fetchJustWatchData } from './justwatch';
-import { checkIfMovieIsOnLocalNetflix } from './netflix';
-import { movieData, viewingOptionData } from './models';
+import cors from "cors";
+import { createCache } from "./cache";
+import { fetchImdbWatchList } from "./imdb";
+import { fetchBechdel } from "./bechdel";
+import { fetchJustWatchData } from "./justwatch";
+import { checkIfMovieIsOnLocalNetflix } from "./netflix";
+import { movieData, viewingOptionData } from "./models";
 
 const app = express();
 
 const cachePromise = createCache({
-  url: process.env.REDIS_URL,
-  writeOnly: process.env.DISABLE_CACHE,
+	url: process.env.REDIS_URL,
+	writeOnly: process.env.DISABLE_CACHE
 });
 if (process.env.DISABLE_CACHE) {
-  console.log('Cache is disabled');
+	console.log("Cache is disabled");
 }
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/api/watchlist', (req, res) => {
-  const userId = req.query.userId;
-  fetchImdbWatchList(userId).then((list) => {
-    res.json({ list });
-  });
+app.get("/api/watchlist", (req, res) => {
+	const userId = req.query.userId;
+	fetchImdbWatchList(userId).then(list => {
+		res.json({ list });
+	});
 });
 
-app.post('/api/movies', (req, res) => {
-  const movies = req.body.movies;
-  Promise.all(movies.map(movie => fetchMovieDetails(movie))).then((movies) => {
-    res.json({ movies });
-  });
+app.post("/api/movies", (req, res) => {
+	const movies = req.body.movies;
+	Promise.all(movies.map(movie => fetchMovieDetails(movie))).then(movies => {
+		res.json({ movies });
+	});
 });
 
 const fetchMovieDetails = movie =>
-  Promise.all([
-    fetchBechdelWithCache(movie.id).catch((error) => {
-      console.log('Bechdel', error);
-      return null;
-    }),
-    fetchJustWatchDataWithCache(
-      movie.id,
-      movie.title,
-      movie.type,
-      movie.releaseDate,
-    ).catch((error) => {
-      console.log('JustWatch', error);
-      return { viewingOptions: null, ratings: null };
-    }),
-    checkIfMovieIsOnLocalNetflixWithCache(movie.id, movie.title, 'is').catch((error) => {
-      console.log('Netflix', error);
-      return { netflixUrl: null };
-    }),
-  ])
-    .then((bechdelRating_justWatch_localNetflixUrl) => {
-      const bechdelRating = bechdelRating_justWatch_localNetflixUrl[0];
+	Promise.all([
+		fetchBechdelWithCache(movie.id).catch(error => {
+			console.log("Bechdel", error);
+			return null;
+		}),
+		fetchJustWatchDataWithCache(
+			movie.id,
+			movie.title,
+			movie.type,
+			movie.releaseDate
+		).catch(error => {
+			console.log("JustWatch", error);
+			return { viewingOptions: null, ratings: null };
+		}),
+		checkIfMovieIsOnLocalNetflixWithCache(movie.id, movie.title, "is").catch(
+			error => {
+				console.log("Netflix", error);
+				return { netflixUrl: null };
+			}
+		)
+	])
+		.then(bechdelRating_justWatch_localNetflixUrl => {
+			const bechdelRating = bechdelRating_justWatch_localNetflixUrl[0];
 
-      const { viewingOptions, ratings } = bechdelRating_justWatch_localNetflixUrl[1];
+			const {
+				viewingOptions,
+				ratings
+			} = bechdelRating_justWatch_localNetflixUrl[1];
 
-      const { netflixUrl } = bechdelRating_justWatch_localNetflixUrl[2];
-      const netflix = netflixUrl
-        ? viewingOptionData({
-          provider: 'netflix',
-          presentationType: 'hd',
-          monetizationType: 'flatrate',
-          url: netflixUrl,
-        })
-        : null;
+			const { netflixUrl } = bechdelRating_justWatch_localNetflixUrl[2];
+			const netflix = netflixUrl
+				? viewingOptionData({
+						provider: "netflix",
+						presentationType: "hd",
+						monetizationType: "flatrate",
+						url: netflixUrl
+					})
+				: null;
 
-      return movieData({
-        ...movie,
-        imdbRating: movie.ratings.imdb,
-        metascore: movie.ratings.metascore,
-        bechdelRating,
-        netflix,
-        rottenTomatoesMeter: ratings && ratings.rottenTomatoesMeter,
-        amazon: viewingOptions && viewingOptions.amazon,
-        itunes: viewingOptions && viewingOptions.itunes,
-        hbo: viewingOptions && viewingOptions.hbo,
-        showtime: viewingOptions && viewingOptions.showtime,
-      });
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+			return movieData({
+				...movie,
+				imdbRating: movie.ratings.imdb,
+				metascore: movie.ratings.metascore,
+				bechdelRating,
+				netflix,
+				rottenTomatoesMeter: ratings && ratings.rottenTomatoesMeter,
+				amazon: viewingOptions && viewingOptions.amazon,
+				itunes: viewingOptions && viewingOptions.itunes,
+				hbo: viewingOptions && viewingOptions.hbo,
+				showtime: viewingOptions && viewingOptions.showtime
+			});
+		})
+		.catch(error => {
+			console.error(error);
+		});
 
-const fetchBechdelWithCache = (imdbId) => {
-  const cacheKey = `bechdel:${imdbId}`;
-  return cachePromise(cacheKey, () => fetchBechdel(imdbId), 30 * 24 * 60 * 60);
+const fetchBechdelWithCache = imdbId => {
+	const cacheKey = `bechdel:${imdbId}`;
+	return cachePromise(cacheKey, () => fetchBechdel(imdbId), 30 * 24 * 60 * 60);
 };
 
-const fetchJustWatchDataWithCache = (imdbId, title, type, releaseDateTimestamp) => {
-  const cacheKey = `justwatch:${imdbId}`;
-  return cachePromise(
-    cacheKey,
-    () => fetchJustWatchData(imdbId, title, type, releaseDateTimestamp),
-    24 * 60 * 60,
-  );
+const fetchJustWatchDataWithCache = (
+	imdbId,
+	title,
+	type,
+	releaseDateTimestamp
+) => {
+	const cacheKey = `justwatch:${imdbId}`;
+	return cachePromise(
+		cacheKey,
+		() => fetchJustWatchData(imdbId, title, type, releaseDateTimestamp),
+		24 * 60 * 60
+	);
 };
 
 const checkIfMovieIsOnLocalNetflixWithCache = (imdbId, title, locale) => {
-  const cacheKey = `netflix:${imdbId}:${locale}`;
-  return cachePromise(
-    cacheKey,
-    () => checkIfMovieIsOnLocalNetflix(imdbId, title, locale),
-    24 * 60 * 60,
-  );
+	const cacheKey = `netflix:${imdbId}:${locale}`;
+	return cachePromise(
+		cacheKey,
+		() => checkIfMovieIsOnLocalNetflix(imdbId, title, locale),
+		24 * 60 * 60
+	);
 };
 
 // process.env.PORT lets the port be set by Heroku
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}!`);
+	console.log(`App listening on port ${port}!`);
 });
